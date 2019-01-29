c68654a103b110548e517367ebf0796928b218a0
Unknown <glenn.dev@hotmail.com>
2019-01-29 11:46:07 +0100

    29-1-2019 11.44 | Check Description
    added buttons to attackmenu, maked a string array for attacks.

--- MonsterBattle/MonsterBattle/AttackMenu.cs
+++ MonsterBattle/MonsterBattle/AttackMenu.cs
@@ -12,9 +12,28 @@ namespace MonsterBattle
 {
     public partial class AttackMenu : UserControl
     {
+        public string[] attacks = new string[18] {
+            "Lightning Strike","Bolder","Speed Attack",
+            "Bulble gum","Flash","Light",
+            "Circle","venon","hide and seek",
+            "pee attack","go sleep","kontgat",
+            "rapest","flying pigs","play minecraft",
+            "go to nine gag","asshole","attack his mom"
+        };
+
         public AttackMenu()
         {
             InitializeComponent();
+        }
+
+        private void AttackMenu_Load(object sender, EventArgs e)
+        {
+            if (pokemon == "Pikachu")
+            {
+                attackOneButton.Text = attacks[3].ToString();
+                attackTwoButton.Text = attacks[10].ToString();
+                attackThreeButton.Text = attacks[6].ToString();
+            }
         }
     }
 }
