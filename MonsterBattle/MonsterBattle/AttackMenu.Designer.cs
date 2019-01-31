bfac28671a37baa98ec52508088548f039db6f90
Glenn <glenn.dev@hotmail.com>
2019-01-31 09:30:13 +0100

    31-1-3019 9.30 | set buttons in a pannel

--- MonsterBattle/MonsterBattle/AttackMenu.Designer.cs
+++ MonsterBattle/MonsterBattle/AttackMenu.Designer.cs
@@ -28,53 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.attackOneButton = new System.Windows.Forms.Button();
-            this.attackThreeButton = new System.Windows.Forms.Button();
-            this.attackTwoButton = new System.Windows.Forms.Button();
-            this.backButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // attackOneButton
-            // 
-            this.attackOneButton.Location = new System.Drawing.Point(3, 3);
-            this.attackOneButton.Name = "attackOneButton";
-            this.attackOneButton.Size = new System.Drawing.Size(235, 34);
-            this.attackOneButton.TabIndex = 1;
-            this.attackOneButton.UseVisualStyleBackColor = true;
-            // 
-            // attackThreeButton
-            // 
-            this.attackThreeButton.Location = new System.Drawing.Point(242, 41);
-            this.attackThreeButton.Name = "attackThreeButton";
-            this.attackThreeButton.Size = new System.Drawing.Size(235, 34);
-            this.attackThreeButton.TabIndex = 2;
-            this.attackThreeButton.UseVisualStyleBackColor = true;
-            // 
-            // attackTwoButton
-            // 
-            this.attackTwoButton.Location = new System.Drawing.Point(242, 3);
-            this.attackTwoButton.Name = "attackTwoButton";
-            this.attackTwoButton.Size = new System.Drawing.Size(235, 34);
-            this.attackTwoButton.TabIndex = 3;
-            this.attackTwoButton.UseVisualStyleBackColor = true;
-            // 
-            // backButton
-            // 
-            this.backButton.Location = new System.Drawing.Point(3, 41);
-            this.backButton.Name = "backButton";
-            this.backButton.Size = new System.Drawing.Size(235, 34);
-            this.backButton.TabIndex = 4;
-            this.backButton.Text = "Back";
-            this.backButton.UseVisualStyleBackColor = true;
             // 
             // AttackMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.backButton);
-            this.Controls.Add(this.attackTwoButton);
-            this.Controls.Add(this.attackThreeButton);
-            this.Controls.Add(this.attackOneButton);
             this.Name = "AttackMenu";
             this.Size = new System.Drawing.Size(480, 78);
             this.Load += new System.EventHandler(this.AttackMenu_Load);
@@ -83,10 +42,5 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button attackOneButton;
-        private System.Windows.Forms.Button attackThreeButton;
-        private System.Windows.Forms.Button attackTwoButton;
-        private System.Windows.Forms.Button backButton;
     }
 }
